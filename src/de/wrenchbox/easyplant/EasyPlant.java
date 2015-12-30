00d5a98d977292ea4b52b270bbc150365065512f
AmShaegar <developer@amshaegar.de>
2015-12-30 21:32:16 +0100

    removed metrics; adapted current version of bukkit

--- src/de/wrenchbox/easyplant/EasyPlant.java
+++ src/de/wrenchbox/easyplant/EasyPlant.java
@@ -1,7 +1,5 @@
 package de.wrenchbox.easyplant;
 
-import java.io.IOException;
-
 import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -13,14 +11,6 @@ public class EasyPlant extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		plugin = this;
-		
-		try {
-			Metrics metrics = new Metrics(this);
-			metrics.start();
-		} catch (IOException e) {
-			getLogger().info("Unable to connect to metrics server.");
-		}
-		
 		new PlantListener();
 	}
 
